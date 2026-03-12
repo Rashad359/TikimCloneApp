8544ca9865a6ec3ab71f8ed16797fe913864caf6
Rashad359 <aliyevrasad07@gmail.com>
2026-03-12 12:30:40 +0400

    transition animation added to otp view

--- Tikim/TikimApp.swift
+++ Tikim/TikimApp.swift
@@ -8,32 +8,91 @@ struct TikimApp: App {
     @AppStorage("logout") private var logout = false
     @AppStorage("language") private var language = "en"
     
+    @State private var logState: LogState
+    
+    init() {
+        let loggedIn = UserDefaults.standard.bool(forKey: "presentMain")
+        let loggedOut = UserDefaults.standard.bool(forKey: "logout")
+        
+        if loggedIn {
+            _logState = State(initialValue: .main)
+        } else if loggedOut {
+            _logState = State(initialValue: .login)
+        } else {
+            _logState = State(initialValue: .onboarding)
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ZStack {
-                if userLoggedIn {
-                    MainView()
-                        .transition(
-                            .asymmetric(
-                                insertion: .move(edge: .trailing),
-                                removal: .move(edge: .trailing)
-                            )
+                switch logState {
+                case .main:
+                    NavigationStack {
+                        MainView()
+                    }
+                    .transition(
+                        .asymmetric(
+                            insertion: .move(edge: .trailing),
+                            removal: .move(edge: .trailing)
                         )
-                } else {
-                    if logout {
-                        NavigationStack {
-                            LoginView()
-                        }
-                    } else {
-                        NavigationStack {
-                            OnboardingView()
-                        }
+                    )
+                case .login:
+                    NavigationStack {
+                        LoginView()
+                    }
+                    .transition(
+                        .asymmetric(
+                            insertion: .move(edge: .leading),
+                            removal: .move(edge: .leading)
+                        )
+                    )
+                case .onboarding:
+                    NavigationStack {
+                        OnboardingView()
                     }
                 }
+                //                if userLoggedIn {
+                //                    MainView()
+                //                        .transition(
+                //                            .asymmetric(
+                //                                insertion: .move(edge: .trailing),
+                //                                removal: .move(edge: .trailing)
+                //                            )
+                //                        )
+                //                } else {
+                //                    if logout {
+                //                        NavigationStack {
+                //                            LoginView()
+                //                        }
+                //                    } else {
+                //                        NavigationStack {
+                //                            OnboardingView()
+                //                        }
+                //                    }
+                //                }
             }
             .environment(\.locale, Locale(identifier: language))
             .animation(.easeInOut(duration: 0.2), value: userLoggedIn)
             .preferredColorScheme(.light)
+            .onChange(of: userLoggedIn) { _, _ in updateLogState() }
+            .onChange(of: logout) { _, _ in updateLogState() }
         }
     }
+    
+    private func updateLogState() {
+        if userLoggedIn {
+            logState = .main
+        } else if logout {
+            logState = .login
+        } else {
+            logState = .onboarding
+        }
+    }
+}
+
+enum LogState {
+    case main
+    case login
+    case onboarding
 }
